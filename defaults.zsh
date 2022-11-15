f755ecaee23f3d1ce772c5d4ef5cbc83e821280c
ravenholt <aravenholt@gmail.com>
2022-11-15 12:43:07 -0500

    Update defaults.zsh

--- defaults.zsh
+++ defaults.zsh
@@ -2,3 +2,4 @@
 defaults write com.apple.dock "autohide" -bool "true"
 defaults write com.apple.dock "autohide-time-modifier" -float ".25"
 defaults write com.apple.dock "autohide-delay" -float "0"
+killall Dock
