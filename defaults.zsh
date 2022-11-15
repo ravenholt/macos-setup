edbf4e47b5681201b421e3149da7d604960f2b49
ravenholt <aravenholt@gmail.com>
2022-11-15 12:42:42 -0500

    Create defaults.zsh
    Defaults settings for MacOS

---
+++ defaults.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "autohide-time-modifier" -float ".25"
+defaults write com.apple.dock "autohide-delay" -float "0"
